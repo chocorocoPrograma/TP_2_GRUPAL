41dad5ed02cd060d22bb3cc560d303b1ab8b8822
chocorocoPrograma <rociodlfv@gmail.com>
2024-11-13 17:15:01 -0300

    creando archivo JS para hacer cartel

---
+++ js/cancelar.js
@@ -0,0 +1,7 @@
+const abrirCartel =
+    document.querySelector("#cartel");
+
+const cerrarCartel =
+    document.querySelector("#cerrarCartel");
+const cartel =
+    document.querySelector("#cartel");
